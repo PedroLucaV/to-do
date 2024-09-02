5406078d05c5c67c0d4237ac8268bcd1393276f6
Pedro Lucas <izielucas2016@gmail.com>
2024-09-02 08:13:03 -0300

    'REF 02 Created'

--- src/controllers/taskControllers.js
+++ src/controllers/taskControllers.js
@@ -13,7 +13,35 @@ export const createTask = async (req, res) => {
     }
     catch (error){
         console.error(error)
-        res.status(400).json({ message: "Erro ao criar tarefa" });
+        res.status(500).json({ message: "Erro ao criar tarefa" });
+    }
+}
+
+//REF 02
+export const getTasksByPage = async (req, res) => {
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 10
+    const offset = (page - 1) * limit
+
+    try {
+        const tarefas = await Tasks.findAndCountAll({
+        limit, offset
+        })
+        const totalPaginas = Math.ceil(tarefas.count / limit)
+        
+        res.status(200).json({
+        totalTarefas: tarefas.count,
+        totalPaginas,
+        paginaAtual: page,
+        itemsPorPagina: limit,
+        proximaPagina: totalPaginas === 0 
+            ? null 
+            : `${process.env.BASE_URL}/tarefas?page=${page + 1}&limit=${limit}`,
+        tarefas: tarefas.rows
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Erro ao buscar tarefas" });
     }
 }
 
@@ -59,6 +87,6 @@ export const updateStatus = async (req, res) => {
         res.status(200).json({tarefa: updatedTask});
     } catch (error) {
         console.error(error)
-        res.status(400).json({ message: "Erro ao atualizar tarefa" });
+        res.status(500).json({ message: "Erro ao atualizar tarefa" });
     }
 }
